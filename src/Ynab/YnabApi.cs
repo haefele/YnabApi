fb4f0bb8250dd786021ca930d24c8801873e6982
Daniel Häfele <haefele@xemio.net>
2015-08-08 19:09:16 +0200

    Fix unused usings.

--- src/Ynab/YnabApi.cs
+++ src/Ynab/YnabApi.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
