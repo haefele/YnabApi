f81ccc0043d00ef6fb0ef4dbe0df74b59e999294
Daniel Häfele <haefele@xemio.net>
2015-08-26 23:23:45 +0200

    Add nuget.exe and ynab.pack.ps1 script to automatically create a nuget package.

---
+++ nuget/Ynab.pack.ps1
@@ -0,0 +1,11 @@
+$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..';
+
+$version = [System.Reflection.Assembly]::LoadFile("$root\src\Ynab\bin\Release\Ynab.dll").GetName().Version;
+$versionAsString = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build);
+
+$nuspecPath = $root + "\nuget\Ynab.nuspec";
+[xml]$content = Get-Content $nuspecPath; 
+$content.package.metadata.version = $versionAsString;
+$content.Save($nuspecPath);
+
+& $root\nuget\nuget.exe pack $nuspecPath;
