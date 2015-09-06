f350a92096543abc126c96650e34e5d1c2fc7448
Daniel Häfele <haefele@xemio.net>
2015-09-06 20:35:19 +0200

    Improve delete-transaction-device-action

--- src/YnabApi/DeviceActions/DeleteTransactionDeviceAction.cs
+++ src/YnabApi/DeviceActions/DeleteTransactionDeviceAction.cs
@@ -9,21 +9,11 @@ namespace YnabApi.DeviceActions
 
         public JObject ToJsonForYdiff(string deviceId, int knowledgeNumber)
         {
-            return new JObject
-            {
-                { "accepted", true },
-                { "accountId", this.Transaction.Account.Id },
-                { "amount", this.Transaction.Amount },
-                { "categoryId", this.Transaction.Category.Id },
-                { "cleared", this.Transaction.Cleared ? "Cleared" : "Uncleared" },
-                { "date", this.Transaction.Date.ToString("yyyy-MM-dd") },
-                { "entityId", this.Transaction.Id },
-                { "entityType", "transaction" },
-                { "entityVersion", $"{deviceId}-{knowledgeNumber}" },
-                { "isTombstone", true },
-                { "memo", this.Transaction.Memo },
-                { "payeeId", this.Transaction.Payee.Id }
-            };
+            var json = this.Transaction.GetJson();
+
+            json["isTombstone"] = true;
+
+            return json;
         }
     }
 }
